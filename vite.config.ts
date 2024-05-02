d599feca11f03e6e45cf33d1b1b3d99412f3772e
DOJO <kevin11272@gmail.com>
2024-05-02 16:08:19 +0800

    add pwa images and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+
+const manifest = {
+  registerType: "autoUpdate",
+  includeAssets: ["**/*"],
+  devOptions: {
+    enabled: true
+  },
+  manifest: {
+    "name": "Weather APP",
+    "short_name": "Weather APP",
+    "description": "Just for fun",
+    "start_url": "/",
+    "display": "standalone",
+    "theme_color": "#ffffff",
+    "background_color": "#ffffff",
+    "icons": [
+      {
+        "src": "/pwa-192x192.png",
+        "sizes": "192x192",
+        "type": "image/png"
+      },
+      {
+        "src": "/pwa-512x512.png",
+        "sizes": "512x512",
+        "type": "image/png"
+      }
+    ]
+  }
+};
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA(manifest),
+  ],
 })
